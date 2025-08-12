05101fb9c887567b479eceeaec8c0d26bac2cecb
RndraThr <rendrabtuharea@gmail.com>
2025-08-13 03:16:26 +0700

    feat: update icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'AERGAS System')</title>
-    <link rel="icon" href="{{ asset('assets/AERGAS_PNG.png') }}" type="image/png">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/png">
 
     <!-- Tailwind CSS -->
     <script src="https://cdn.tailwindcss.com"></script>
