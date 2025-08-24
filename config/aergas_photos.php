1b85bfa75b9eff74ced703826c4814216f40410a
RndraThr <rendrabtuharea@gmail.com>
2025-08-24 15:22:25 +0700

    feat: update promtping

--- config/aergas_photos.php
+++ config/aergas_photos.php
@@ -261,7 +261,7 @@ return [
                     'prompt' => 'Analisis foto pondasi dengan penilaian SKOR (0-100):
 
                                 ELEMEN YANG DIPERIKSA:
-                                1. "Gambar ini menampilkan sebuah pipa berwarna kuning yang berdiri tegak.Bagian bawah pipa tersebut tertancap atau menempel pada pondasi berbentuk balok berwarna abu-abu. Pondasi terlihat kokoh dan berfungsi sebagai penopang pipa kuning agar tetap berdiri. fokuskan pada pondasi itu sendiri, bukan keseluruhan pipa." (60 poin) -
+                                1. "Gambar ini menampilkan sebuah pipa berwarna kuning yang berdiri tegak.Bagian bawah pipa tersebut tertancap atau menempel pada pondasi berbentuk balok berwarna abu-abu. fokusnya pada pondasi berbentuk balok berwarna abu2. abaikan dokumeen tagging, jika ada pipa kuning tetapi pondasi balok berwarna abu2 tidak terlihat maka gagal" (60 poin) -
                                 2. KUALITAS FOTO (40 poin) - Pada bagian pondasi memang sedikit blur tetapi warna dan bentuk masih bisa terlihat. anggap pondasi berhasil teridentifikasi
 
                                 PANDUAN PENILAIAN:
